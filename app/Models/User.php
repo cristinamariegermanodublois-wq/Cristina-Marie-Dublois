74d0c9431d88551122e1350d8c8906a74f02a9a1
angelyn <angelynespinosa585@gmail.com>
2026-01-30 12:33:23 +0800

    Customize users table migration and model

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,6 +21,8 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'role',
+        'is_active',
     ];
 
     /**
@@ -41,5 +43,7 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
+        'is_active' => 'boolean',
+        
     ];
 }
